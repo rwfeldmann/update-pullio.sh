5fa71ee38b624ce1a9794e227a2f57ef6ff8ef6b
rwfeldmann <richard.feldmann@gmail.com>
2025-05-30 14:01:11 -0600

    Update update-pullio.sh
    Added execute perms check

--- update-pullio.sh
+++ update-pullio.sh
@@ -9,6 +9,11 @@ EXITVALUE=$?
 if [ $EXITVALUE != 0 ]; then
     /usr/bin/logger -t update-pullio "ALERT exited abnormally with [$EXITVALUE]"
 else
+    # Check if pullio is executable by all users
+    if [ ! -x /usr/local/bin/pullio ]; then
+        chmod a+x /usr/local/bin/pullio
+        /usr/bin/logger -t update-pullio "INFO set executable permission on /usr/local/bin/pullio"
+    fi
     /usr/bin/logger -t update-pullio "INFO ran successfully with [$EXITVALUE]"
 fi
 
